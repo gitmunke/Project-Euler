2d8647fcca95a8f4de371f51c49cc2b433bf364c
Armando J Wood <armando.wood@gmail.com>
2022-01-01 18:49:28 -0800

    answer 21

--- 021.py
+++ 021.py
@@ -8,3 +8,25 @@
 #
 # Evaluate the sum of all the amicable numbers under 10000.
 
+def sum_proper_divisors(inp):
+    inp = int(inp)
+    final = []
+    for div in range(1, inp // 2 + 1):
+        if inp % div == 0:
+            final.append(div)
+    return sum(final)
+
+
+stor = []
+
+for inp in range(1, 10000):
+    if inp in stor:
+        continue
+    pair = sum_proper_divisors(inp)
+    if sum_proper_divisors(pair) == inp:
+        if inp != pair:
+            stor.append(inp)
+            stor.append(pair)
+            print(inp, pair)
+
+print(sum(stor))
