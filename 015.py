391e6e95edb0059bac5018e5bbf341aa75da0fc6
Armando J Wood <armando.wood@gmail.com>
2022-01-01 18:37:11 -0800

    answer 15

--- 015.py
+++ 015.py
@@ -2,3 +2,16 @@
 # 6 routes to the bottom right corner.
 #
 # How many such routes are there through a 20×20 grid?
+
+import itertools
+
+r = str(0)
+d = str(1)
+
+moves = [r, r, r, r, r, r, r, r, r, r, d, d, d, d, d, d, d, d, d, d]
+perm = itertools.permutations(moves)
+
+for i in set(perm):
+    print(i)
+
+#got answer with a combination calculator, my computer couldnt handle it
