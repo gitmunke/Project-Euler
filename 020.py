8f519a40b6749ba967120de5d98723681321c4ff
Armando J Wood <armando.wood@gmail.com>
2022-01-01 18:49:01 -0800

    moved

---
+++ 020.py
@@ -0,0 +1,28 @@
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
+def factorial(inp):
+    inp = int(inp)
+    total = 1
+    for n in range(1, inp + 1):
+        total *= n
+    return total
+
+
+def digit_sum(inp):
+    total = 0
+    inp = str(inp)
+    for digit in range(len(inp)):
+        total += int(inp[digit])
+    return total
+
+
+butt = False
+while butt != True:
+    c = input('gimme a numbah! ')
+    print(digit_sum(factorial(c)))
+    
